a4cf0197cb7246722c815c4d15b444c6d8545675
Aliaksandr Kolos <kolosovskii.201416@gmail.com>
2024-01-09 19:57:30 +0300

    HomeWork9

---
+++ HomeWork9/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String pathToFile = "D:\\test\\HomeWork\\HomeWork9\\file";
+        String resultFile = "result.txt";
+
+
+        File inputDirectory = new File(pathToFile);
+        File[] files = inputDirectory.listFiles();
+
+        if (files != null) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile))) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                writer.write(line + " ");
+                            }
+                        }
+                    }
+                }
+                System.out.println("Копирование завершено");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("файлы отсутствуют");
+        }
+    }
+}
+
